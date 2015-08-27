a3e8ec2f52f4b8384f42a77e77a95df753b39d38
XaviProva <xavsanmo@hotmail.com>
2015-08-27 10:24:28 +0200

    codi base

---
+++ codi_base.php
@@ -0,0 +1,36 @@
+ <!DOCTYPE HTML>
+<html>
+<head>
+
+</head>
+<body>
+
+<?php
+// define variables and set to empty values
+$nameErr = $emailErr = $genderErr = $websiteErr = "";
+$name = $email = $gender = $comment = $website = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+}
+
+function test_input($data) {
+   $data = trim($data);
+   $data = stripslashes($data);
+   $data = htmlspecialchars($data);
+   return $data;
+}
+?>
+
+
+<?php
+
+echo $website;
+echo "<br>";
+echo $comment;
+echo "<br>";
+echo $gender;
+?>
+
+</body>
+</html>
